f2b32c2aa787c6a21d7b5e243ff375179f155f94
AdarshNaidu <naidu.adarsh2511@gmail.com>
2020-01-23 23:43:08 +0530

    templating engine setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.set('view engine', 'hbs');
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, './Templates/views');
+
+app.use(express.static(publicDirectoryPath));
+app.set('views', viewsPath);
 
 app.get('/', (req, res) => {
     res.send("Server connection successful")
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
 })
 
 app.listen(PORT, (error, resp) => {
